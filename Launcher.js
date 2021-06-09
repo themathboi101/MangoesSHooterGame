488b87927477e3dd0f7db0b5923af4fa0a5e1f73
themathboi101 <78664310+themathboi101@users.noreply.github.com>
2021-06-09 15:34:17 -0400

    Update Launcher.js

--- Launcher.js
+++ Launcher.js
@@ -20,14 +20,14 @@ class Launcher{
     }
 
     display(){
-        image(this.launcher,200,20);
+       
         
         if(this.launcher.bodyA){
             var pointA = this.launcher.bodyA.position;
             var pointB = this.pointB;
             push();
-            line(pointA.x - 20, pointA.y, pointB.x + 30, pointB.y - 3);
-            image(this.launcher,pointA.x -30, pointA.y -10,15,30);
+            line(pointA.x , pointA.y, pointB.x , pointB.y );
+            
         } 
             
     }
